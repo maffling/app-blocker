5067440a14abf03f8586784917de0860d6e9ea56
maffling <maffling@proton.me>
2024-01-09 22:31:38 +0000

    Initial Commit

---
+++ app-blocker/app_blockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_blockerApp.swift
+//  app-blocker
+//
+//  Created by Darren on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct app_blockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
